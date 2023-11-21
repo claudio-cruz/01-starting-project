c294267b6630a2c09e4dd261237aebea50619b9a
Claudio Cruz <claudiocruz1989@gmail.com>
2023-11-21 18:34:35 +0000

    Lift up the userInput state and handleChange function to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import React from "react"
+import React, { useState } from "react"
 import Header from "./Components/Header"
 import UserInput from "./Components/UserInput"
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  })
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput(prevUserInput => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue
+      }
+    })
+  }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput userInput={userInput} onChange={handleChange}/>
     </>
   )
 }
